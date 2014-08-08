e1378926d0c407306dac3d1999cc1a32e977c5e9
Benjamin Randles-Dunkley <benrd@quillcontent.com>
2014-08-08 16:03:57 +0100

    Add augmentations plugin to lib/augmentations

--- lib/augmentations.rb
+++ lib/augmentations.rb
@@ -1,5 +1,14 @@
 require "augmentations/version"
 
-module Augmentations
-  # Your code goes here...
+class ::Object
+  def self.augment(*mods)
+    include *mods
+    mods.each {|mod| class_eval &mod.augmentation }
+  end
+end
+
+class ::Module
+  def augmentation(&block)
+    @augmentation ||= block
+  end
 end
